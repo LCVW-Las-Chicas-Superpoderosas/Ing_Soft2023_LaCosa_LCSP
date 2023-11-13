dfabd5a86e3e8a99bc729aed8feac13f03ae1d7c
klartz <kurtzlara00@gmail.com>
2023-11-13 20:10:09 -0300

    feat: map each card name to its id

--- frontend/src/services/cardConditions.js
+++ frontend/src/services/cardConditions.js
@@ -28,11 +28,53 @@ const getCardName = (cardToken) => {
 		case cardID >= 22 && cardID <= 26:
 			return 'lanzallamas';
 
+		case cardID >= 27 && cardID <= 29:
+			return 'analisis';
+
+		case cardID >= 30 && cardID <= 31:
+			return 'hacha';
+
+		case cardID >= 32 && cardID <= 39:
+			return 'sospecha';
+
+		case cardID >= 40 && cardID <= 42:
+			return 'whisky';
+
+		case cardID >= 43 && cardID <= 47:
+			return 'determinacion';
+
 		case cardID >= 48 && cardID <= 49:
 			return 'vigila tus espaldas';
 
 		case cardID >= 50 && cardID <= 54:
 			return 'cambio de lugar';
+
+		case cardID >= 55 && cardID <= 59:
+			return 'mas vale que corras';
+
+		case cardID >= 60 && cardID <= 66:
+			return 'seduccion';
+
+		case cardID >= 67 && cardID <= 70:
+			return 'aterrador';
+
+		case cardID >= 71 && cardID <= 73:
+			return 'aqui estoy bien';
+
+		case cardID >= 74 && cardID <= 77:
+			return 'no, gracias';
+
+		case cardID >= 78 && cardID <= 80:
+			return 'fallaste';
+
+		case cardID >= 81 && cardID <= 83:
+			return 'nada de barbacoas';
+
+		case cardID >= 84 && cardID <= 85:
+			return 'cuarentena';
+
+		case cardID >= 86 && cardID <= 88:
+			return 'puerta atrancada';
 
 		default:
 			return null;
