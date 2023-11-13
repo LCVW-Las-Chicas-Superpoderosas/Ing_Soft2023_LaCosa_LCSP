9562dbefd13fb4540e557028fc11d85fd940fc0b
Tomas Bazan <tomas.pablo.bazan@mi.unc.edu.ar>
2023-11-13 20:43:46 -0300

    feat: defense stage

--- frontend/src/components/Game/EndTurnExchange.jsx
+++ frontend/src/components/Game/EndTurnExchange.jsx
@@ -8,12 +8,15 @@ import {
 	ModalHeader,
 	ModalFooter,
 	ModalBody,
+	HStack,
+	Box,
 } from '@chakra-ui/react';
 import Hand from '../Hand/Hand';
 import {useSelector, useDispatch} from 'react-redux';
 import PropTypes from 'prop-types';
 import {setHand} from '../../appActions';
 import {v4 as uuidv4} from 'uuid';
+import Card from '../Card/Card';
 
 const pickFirstCard = 1;
 const pickDefense = 2;
@@ -27,34 +30,31 @@ const EndTurnExchange = ({onOpen, isOpen, onClose, etapa, setEtapa}) => {
 	const nextPlayerId = useSelector((state) => state.game.nextPlayerId);
 	const dispatch = useDispatch();
 	const [attackerPlayer, setAttackerPlayer] = useState(null);
+	const [defenseCards, setDefenseCards] = useState([]);
 
 	useEffect(() => {
 		const connection = new WebSocket(
 			`ws://localhost:8000/ws/card_exchange?id_player=${idPlayer}`,
 		);
+		console.log('set Etapa ', etapa);
 		setSocket(connection);
 		connection.onopen = () => {
 			const idToSend = {type: 'connect', id_player: idPlayer};
 			connection.send(JSON.stringify(idToSend));
 		};
-
+		console.log('etapaaaa: ', etapa);
 		if (connection) {
 			connection.onmessage = function (event) {
 				const message = JSON.parse(event.data);
-				console.log('In message, recieved a messasge:', message);
 				if (message.data.type === 'exchange_offert') {
 					console.log('Recieved a exchange_offert: ', message.data);
 					const attacker = {
 						id: message.data.attacker_id,
 						attackerName: message.data.attacker_name,
 					};
-					console.log('ATACKEEEEER', attacker.id);
-					console.log('Response:', message.data);
 					setAttackerPlayer(attacker);
 					setEtapa(pickSecondCard);
-				}
-				// exchange_result
-				else if (message.data.type === 'result') {
+				} else if (message.data.type === 'result') {
 					console.log('Recieved a get_result: ', message.data);
 					const newHand = message.data.hand.map((card) => ({
 						id: uuidv4(),
@@ -63,10 +63,21 @@ const EndTurnExchange = ({onOpen, isOpen, onClose, etapa, setEtapa}) => {
 					}));
 					dispatch(setHand(newHand));
 					setEtapa(showNewHand);
-				}
-				// defensa
-				else if (message.type === 'defense') {
+				} else if (message.data.type === 'defense') {
 					console.log('Recieved a defense: ', message.data);
+					const defenseHand = message.data.defense_cards.map((card) => ({
+						id: uuidv4(),
+						token: card,
+					}));
+					const attacker = {
+						id: message.data.attacker_id,
+						attackerName: message.data.attacker_name,
+					};
+
+					console.log('ATACKEEEEER', attacker.id);
+					setAttackerPlayer(attacker);
+
+					setDefenseCards(defenseHand);
 					setEtapa(pickDefense);
 				} else {
 					console.log('Recieved a message without type: ', message.data);
@@ -79,7 +90,7 @@ const EndTurnExchange = ({onOpen, isOpen, onClose, etapa, setEtapa}) => {
 				}
 			};
 		}
-	}, [idPlayer, etapa, attackerPlayer, setEtapa, dispatch]);
+	}, [idPlayer, etapa, attackerPlayer, setEtapa, dispatch, defenseCards]);
 
 	const handleClick = async () => {
 		if (socket) {
@@ -107,12 +118,46 @@ const EndTurnExchange = ({onOpen, isOpen, onClose, etapa, setEtapa}) => {
 			}
 		}
 	};
+	function handleDefense(card) {
+		if (socket) {
+			const defenseMessage = {
+				content: {
+					type: 'defense',
+					card_token: card.token,
+					target_id: attackerPlayer.id,
+				},
+			};
+			console.log(JSON.stringify(defenseMessage));
+			socket.send(JSON.stringify(defenseMessage));
+			setEtapa(4);
+		}
+	}
 	function whatToRender(etapa) {
 		if (etapa === pickFirstCard || etapa === showNewHand) {
 			onOpen();
 			return <Hand />;
 		} else if (etapa === pickDefense) {
-			// check what to return
+			onOpen();
+			console.log('defenseCards: antes del render ', defenseCards);
+			return (
+				<HStack justify='center' maxH='full' minH='full'>
+					{defenseCards?.map((card) => (
+						<Box
+							key={card.id}
+							width='170px' // Set the width to control the card size
+							height='200px' // Set the height to control the card size
+							data-testid='handCard'
+						>
+							<Card
+								key={card.id}
+								onClick={() => handleDefense(card)}
+								info={card}
+								front={true}
+							/>
+						</Box>
+					))}
+				</HStack>
+			);
 		} else if (etapa === pickSecondCard) {
 			onOpen();
 			return <Hand isExchange={true} />;
@@ -126,7 +171,7 @@ const EndTurnExchange = ({onOpen, isOpen, onClose, etapa, setEtapa}) => {
 		} else if (etapa === pickSecondCard) {
 			return `Player ${attackerPlayer.attackerName} want to exchange: Pick a card`;
 		} else if (etapa === pickDefense) {
-			return 'Pick a card to defend';
+			return `Player ${attackerPlayer.attackerName} Pick a card to defend`;
 		} else if (etapa === showNewHand) {
 			return 'Your new hand';
 		} else {
@@ -148,6 +193,7 @@ const EndTurnExchange = ({onOpen, isOpen, onClose, etapa, setEtapa}) => {
 					<Button bg='green.300' onClick={handleClick}>
 						Exchange
 					</Button>
+
 					<Button
 						colorScheme='red'
 						variant='ghost'
